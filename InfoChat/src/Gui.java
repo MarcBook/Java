5f702297360a206e71ca0fcbfa26354eb9f9474f
MarcBook <marc.o.negele@gmail.com>
2014-09-22 16:40:44 +0200

    first work on GUI.

--- InfoChat/src/Gui.java
+++ InfoChat/src/Gui.java
@@ -1,4 +1,13 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class Gui extends JFrame{
@@ -6,6 +15,24 @@ public class Gui extends JFrame{
 		setTitle("Chat");
 		setSize(200, 400);
 		
+		//START: Gui-Components
+		
+		JLabel messages = new JLabel("Keine Nachrichten verfügbar");
+		
+		JTextField inputMessage = new JTextField();
+		inputMessage.setColumns(15);
+		
+		JButton sendMessage = new JButton("Senden");
+		
+		//END: Gui-Components
+		
+		//START: Adding Components to GUI 
+		add(messages);
+		add(inputMessage);
+		add(sendMessage);
+		//END: Adding Components to GUI 
+		
+		setLayout(new FlowLayout());
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
