767f179cc46283483a878f7d4d3003b1012d57b6
MarcBook <marc.o.negele@gmail.com>
2014-09-23 16:44:15 +0200

    chanch

--- Basti/src/ForSchleife.java
+++ Basti/src/ForSchleife.java
@@ -1,8 +1,15 @@
 
 public class ForSchleife {
 	public static void main(String[] args) {
-		for(int i = 0; i <= 10; i++){
-			System.out.println(i);
+		
+		String [] namen = new String [4];
+		namen[0] = "Basti";
+		namen[1] = "Marc";
+		namen[2] = "KP";
+		namen[3] = "What ever";
+		
+		for(int i = 0; i <= 3; i++){
+			System.out.println(namen[i]);
 		}
 	}
 }
